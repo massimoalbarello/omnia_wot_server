9723d5e1b86f388c31ebf7b2136d6af53045e285
massimoalbarello <massimo00albarello@gmail.com>
2023-02-28 18:18:02 +0100

    send POST request with signed message to omnia backend canister

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+Secp256k1KeyIdentity = require("@dfinity/identity-secp256k1").Secp256k1KeyIdentity;
+fetch = require("isomorphic-fetch");
+
 Servient = require("@node-wot/core").Servient;
 HttpServer = require("@node-wot/binding-http").HttpServer;
 
 WotDevice = require("./dist/base.js").WotDevice;
+
+const identity = Secp256k1KeyIdentity.generate();
+console.log(identity._publicKey);
+
+const message = 'Hello, world!';
+const signature = identity.sign(message);
+console.log(signature);
+
+const body = {
+    "public_key": "public key",
+    "signature": "signature",
+    "message": message
+};
+
+fetch('omnia_backend_canister_url', {
+    method: "POST",
+    headers: {
+        "x-real-ip": "127.0.0.1"    // this header is generated automatically by the boundary node when canister is deployed on the IC. Set it only for testing
+    },
+    body: JSON.stringify(body),
+})
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.text();
+    })
+    .then(function(res) {
+        console.log(res);
+    });
 
 var httpServer = new HttpServer({ port: 8080 });
 
